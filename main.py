ce090ef4225f97262cf0252d077fb2a838764d9a
DevHyung <khuphj@gmail.com>
2018-08-13 15:35:34 +0900

    Iter 결과뽑는데까지

--- main.py
+++ main.py
@@ -5,39 +5,36 @@ import time
 driver = webdriver.Chrome('./chromedriver')
 driver.maximize_window()
 driver.get('https://www.fangraphs.com/tools/wpa-inquirer')
+DELAY = 5
 
 for i in range(1,9): # 8개
     # 첫번째
     driver.find_element_by_xpath('//*[@id="rcbRun_Input"]').click()
     time.sleep(1)
     driver.find_element_by_xpath('//*[@id="rcbRun_DropDown"]/div/ul/li[{}]'.format(i)).click() # 첫번쨰
-    time.sleep(2)
 
+    time.sleep(DELAY)
 
-    input("넘어가려면클릭")
     # 두번쨰
     for twoIdx in range(1,9): #8개
         driver.find_element_by_xpath('//*[@id="rcbBase_Input"]').click()
         time.sleep(1)
         driver.find_element_by_xpath('//*[@id="rcbBase_DropDown"]/div/ul/li[{}]'.format(twoIdx)).click()
-        time.sleep(2)
-        input("넘어가려면클릭")
+        time.sleep(DELAY)
 
         # 세번째
         for threeIdx in range(1, 19):  # 8개
             driver.find_element_by_xpath('//*[@id="rcbInning_Input"]').click()
             time.sleep(1)
             driver.find_element_by_xpath('//*[@id="rcbInning_DropDown"]/div/ul/li[{}]'.format(threeIdx)).click()
-            time.sleep(2)
-            input("넘어가려면클릭")
+            time.sleep(DELAY)
 
             # 네번째 rcbOuts_Input
             for fourIdx in range(1, 4):  # 3개
                 driver.find_element_by_xpath('//*[@id="rcbOuts_Input"]').click()
                 time.sleep(1)
                 driver.find_element_by_xpath('//*[@id="rcbOuts_DropDown"]/div/ul/li[{}]'.format(fourIdx)).click()
-                time.sleep(2)
-                input("넘어가려면클릭")
+                time.sleep(DELAY)
 
                 # 다섯번째 rcbScore_Input
                 for fiveIdx in range(1, 22):  # 21개
@@ -45,8 +42,7 @@ for i in range(1,9): # 8개
                     time.sleep(1)
                     driver.find_element_by_xpath(
                         '//*[@id="rcbScore_DropDown"]/div/ul/li[{}]'.format(fiveIdx)).click()
-                    time.sleep(2)
-                    input("넘어가려면클릭")
+                    time.sleep(DELAY)
                     #=== TEST PRINT
                     bs4 = BeautifulSoup(driver.page_source, 'lxml')
                     runEnv = bs4.find('input', id='rcbRun_Input')['value']
@@ -55,6 +51,8 @@ for i in range(1,9): # 8개
                     outs = bs4.find('input', id='rcbOuts_Input')['value']
                     runDifferential = bs4.find('input', id='rcbScore_Input')['value']
                     print(runEnv,baseSitu,inning,outs,runDifferential)
+                    print(driver.find_element_by_xpath('//*[@id="radAjax"]/table/tbody/tr[6]/td[2]').text)
+                    print(driver.find_element_by_xpath('//*[@id="radAjax"]/table/tbody/tr[8]/td[2]').text)
 
 
 driver.quit()
