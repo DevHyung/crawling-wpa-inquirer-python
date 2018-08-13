c34f0fbe0e991cfd3183dfed56e215d204879d41
DevHyung <khuphj@gmail.com>
2018-08-13 16:01:53 +0900

    Iter 1차 마감

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from openpyxl import load_workbook
 import time
+import os
+
 driver = webdriver.Chrome('./chromedriver')
 driver.maximize_window()
 driver.get('https://www.fangraphs.com/tools/wpa-inquirer')
@@ -13,6 +16,28 @@ def click_by_xpath(_xpath):
             break
         except:
             time.sleep(0.5)
+def save_excel(_filename,_datalist):
+    FILENAME = './'+_filename
+    if os.path.isfile(FILENAME):  # 파일있는 경우
+        wb = load_workbook(filename=FILENAME)
+        sheet1 = wb[wb.sheetnames[0]]
+        sheet1.append(_datalist)
+        wb.save(FILENAME)
+    else:  # 파일 없는 경우
+        # 엑셀파일 초기설정
+        book = Workbook()
+        # 시트 설정
+        sheet1 = book.active
+        sheet1.column_dimensions['A'].width = 10
+        sheet1.column_dimensions['B'].width = 10
+        sheet1.column_dimensions['C'].width = 10
+        sheet1.column_dimensions['D'].width = 10
+
+        # 저장
+        sheet1.append(['RunEnvironment', 'BaseSituation', 'Inning', 'Outs', 'Minus10', 'Minus9', 'Minus8', 'Minus7', 'Minus6', 'Minus5', 'Minus4', 'Minus3', 'Minus2', 'Minus1', 'Zero', 'Plus1', 'Plus2', 'Plus3', 'Plus4', 'Plus5', 'Plus6', 'Plus7', 'Plus8', 'Plus9', 'Plus10'])
+        sheet1.append(_datalist)
+        book.save(FILENAME)
+
 for i in range(1,9): # 8개
     # 첫번째
     driver.find_element_by_xpath('//*[@id="rcbRun_Input"]').click()
@@ -43,7 +68,8 @@ for i in range(1,9): # 8개
                 time.sleep(1)
                 driver.find_element_by_xpath('//*[@id="rcbOuts_DropDown"]/div/ul/li[{}]'.format(fourIdx)).click()
                 #time.sleep(DELAY)
-
+                homedatalist = []
+                leveragedatalist = []
                 # 다섯번째 rcbScore_Input
                 for fiveIdx in range(1, 22):  # 21개
                     #driver.find_element_by_xpath('//*[@id="rcbScore_Input"]').click()
@@ -62,9 +88,19 @@ for i in range(1,9): # 8개
                     inning = bs4.find('input', id='rcbInning_Input')['value']
                     outs = bs4.find('input', id='rcbOuts_Input')['value']
                     runDifferential = bs4.find('input', id='rcbScore_Input')['value']
-                    print(runEnv,baseSitu,inning,outs,runDifferential)
-                    print(driver.find_element_by_xpath('//*[@id="radAjax"]/table/tbody/tr[6]/td[2]').text)
-                    print(driver.find_element_by_xpath('//*[@id="radAjax"]/table/tbody/tr[8]/td[2]').text)
-
-
+                    homeExp = driver.find_element_by_xpath('//*[@id="radAjax"]/table/tbody/tr[6]/td[2]').text.strip()
+                    Leverage =driver.find_element_by_xpath('//*[@id="radAjax"]/table/tbody/tr[8]/td[2]').text.strip()
+                    print(runEnv,baseSitu,inning,outs,runDifferential,homeExp,Leverage)
+                    if len(homedatalist) == 0:
+                        homedatalist.extend([runEnv,baseSitu,inning,outs,runDifferential,homeExp])
+                    else:
+                        homedatalist.append(homeExp)
+                    if len(leveragedatalist) == 0:
+                        leveragedatalist.extend([runEnv,baseSitu,inning,outs,runDifferential,Leverage])
+                    else:
+                        leveragedatalist.append(Leverage)
+                print(homedatalist)
+                save_excel('home.xlsx',homedatalist)
+                print(leveragedatalist)
+                save_excel('leverage.xlsx', leveragedatalist)
 driver.quit()
