3c5346eb1951d7f8c94cb995384d6b04cbf2d0fc
DevHyung <khuphj@gmail.com>
2018-08-13 14:28:43 +0900

    Iterator 2번째 옵션

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ driver = webdriver.Chrome('./chromedriver')
 driver.maximize_window()
 driver.get('https://www.fangraphs.com/tools/wpa-inquirer')
 
-for i in range(1,9):
+for i in range(1,9): # 8개
+    # 첫번째
     driver.find_element_by_xpath('//*[@id="rcbRun_Input"]').click()
     time.sleep(1)
-    driver.find_element_by_xpath('//*[@id="rcbRun_DropDown"]/div/ul/li[{}]'.format(i)).click()
+    driver.find_element_by_xpath('//*[@id="rcbRun_DropDown"]/div/ul/li[{}]'.format(i)).click() # 첫번쨰
     time.sleep(2)
-    bs4 = BeautifulSoup(driver.page_source,'lxml')
-    runEnv = bs4.find('input',id='rcbRun_Input')['value']
-    print(runEnv)
+
+    input("넘어가려면클릭")
+    # 두번쨰
+    for twoIdx in range(1,9): #8개
+        driver.find_element_by_xpath('//*[@id="rcbBase_Input"]').click()
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="rcbBase_DropDown"]/div/ul/li[{}]'.format(twoIdx)).click()
+        time.sleep(2)
+        input("넘어가려면클릭")
+        bs4 = BeautifulSoup(driver.page_source, 'lxml')
+        runEnv = bs4.find('input', id='rcbRun_Input')['value']
+        baseSitu = bs4.find('input',id='rcbBase_Input')['value']
+        print(runEnv,baseSitu)
 
 
 driver.quit()
