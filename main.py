03e7b110764c1794e338b1b81cd6d775d38c61fb
DevHyung <khuphj@gmail.com>
2018-08-13 15:44:07 +0900

    Modify 클릭부분

--- main.py
+++ main.py
@@ -5,44 +5,56 @@ import time
 driver = webdriver.Chrome('./chromedriver')
 driver.maximize_window()
 driver.get('https://www.fangraphs.com/tools/wpa-inquirer')
-DELAY = 5
-
+DELAY = 3
+def click_by_xpath(_xpath):
+    while True:
+        try:
+            driver.find_element_by_xpath(_xpath).click()
+            break
+        except:
+            time.sleep(0.5)
 for i in range(1,9): # 8개
     # 첫번째
     driver.find_element_by_xpath('//*[@id="rcbRun_Input"]').click()
     time.sleep(1)
     driver.find_element_by_xpath('//*[@id="rcbRun_DropDown"]/div/ul/li[{}]'.format(i)).click() # 첫번쨰
-
-    time.sleep(DELAY)
+    #time.sleep(DELAY)
 
     # 두번쨰
     for twoIdx in range(1,9): #8개
-        driver.find_element_by_xpath('//*[@id="rcbBase_Input"]').click()
+        #driver.find_element_by_xpath('//*[@id="rcbBase_Input"]').click()
+        click_by_xpath('//*[@id="rcbBase_Input"]')
         time.sleep(1)
         driver.find_element_by_xpath('//*[@id="rcbBase_DropDown"]/div/ul/li[{}]'.format(twoIdx)).click()
-        time.sleep(DELAY)
+        #time.sleep(DELAY)
 
         # 세번째
         for threeIdx in range(1, 19):  # 8개
-            driver.find_element_by_xpath('//*[@id="rcbInning_Input"]').click()
+            #driver.find_element_by_xpath('//*[@id="rcbInning_Input"]').click()
+            click_by_xpath('//*[@id="rcbInning_Input"]')
             time.sleep(1)
             driver.find_element_by_xpath('//*[@id="rcbInning_DropDown"]/div/ul/li[{}]'.format(threeIdx)).click()
-            time.sleep(DELAY)
+            #time.sleep(DELAY)
 
             # 네번째 rcbOuts_Input
             for fourIdx in range(1, 4):  # 3개
-                driver.find_element_by_xpath('//*[@id="rcbOuts_Input"]').click()
+                #driver.find_element_by_xpath('//*[@id="rcbOuts_Input"]').click()
+                click_by_xpath('//*[@id="rcbOuts_Input"]')
                 time.sleep(1)
                 driver.find_element_by_xpath('//*[@id="rcbOuts_DropDown"]/div/ul/li[{}]'.format(fourIdx)).click()
-                time.sleep(DELAY)
+                #time.sleep(DELAY)
 
                 # 다섯번째 rcbScore_Input
                 for fiveIdx in range(1, 22):  # 21개
-                    driver.find_element_by_xpath('//*[@id="rcbScore_Input"]').click()
+                    #driver.find_element_by_xpath('//*[@id="rcbScore_Input"]').click()
+                    click_by_xpath('//*[@id="rcbScore_Input"]')
                     time.sleep(1)
                     driver.find_element_by_xpath(
                         '//*[@id="rcbScore_DropDown"]/div/ul/li[{}]'.format(fiveIdx)).click()
-                    time.sleep(DELAY)
+
+                    click_by_xpath('//*[@id="rcbScore_Input"]')
+                    click_by_xpath('//*[@id="rcbScore_Input"]')
+                    time.sleep(0.1)
                     #=== TEST PRINT
                     bs4 = BeautifulSoup(driver.page_source, 'lxml')
                     runEnv = bs4.find('input', id='rcbRun_Input')['value']
